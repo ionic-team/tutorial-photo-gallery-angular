9212c5f3defe341c004e7204400ffe440b214dbc
Matt Netkow <matt.netkow@gmail.com>
2019-12-02 19:36:07 -0600

    web test

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -30,38 +30,43 @@ export class PhotoService {
   public async takePhoto() {
     // Take a photo
     const capturedPhoto = await Camera.getPhoto({
-      resultType: CameraResultType.Uri, // file-based data; provides best performance
+      resultType: CameraResultType.Base64, // file-based data; provides best performance
       source: CameraSource.Camera, // automatically take a new photo with the camera
       quality: 100 // highest quality (0 to 100)
     });
-    
+    console.log(capturedPhoto);
     const savedImageFile = await this.savePicture(capturedPhoto);
 
     // Add new photo to Photos array
     this.photos.unshift({
       filepath: savedImageFile,
+      //  http://localhost:8100/DATA/1575336859585.jpeg 404
       webviewPath: Capacitor.convertFileSrc(savedImageFile)
     });
 
-    // Cache all photo data for future retrieval
-    Storage.set({
-      key: this.PHOTO_STORAGE,
-      value: JSON.stringify(this.photos)
-    });
+    // // Cache all photo data for future retrieval
+    // Storage.set({
+    //   key: this.PHOTO_STORAGE,
+    //   value: JSON.stringify(this.photos)
+    // });
   }
 
   // Save picture to file on device
   private async savePicture(cameraPhoto: CameraPhoto) {
+
+    const readFile = await this.handleWeb(cameraPhoto);
+
     // Read the file into its base64 version
-    const readFile = await Filesystem.readFile({
-      path: cameraPhoto.path
-    });
+    // const readFile = await Filesystem.readFile({
+    //   path: cameraPhoto.path
+    // });
 
     // Write the file to the data directory (instead of temp storage)
     const fileName = new Date().getTime() + '.jpeg';
     await Filesystem.writeFile({
       path: fileName,
-      data: readFile.data,
+      //data: readFile.data,
+      data: readFile,
       directory: FilesystemDirectory.Data
     });
 
@@ -70,8 +75,25 @@ export class PhotoService {
       directory: FilesystemDirectory.Data,
       path: fileName
     });
+    // WEB uri: "/DATA/1575336434117.jpeg"
+    console.log(fileUri); 
 
     return fileUri.uri;
+  }
+
+  private async handleWeb(cameraPhoto) : Promise<string> {
+    return cameraPhoto.base64String;
+    // const file = await fetch(cameraPhoto.webPath);
+    // const readFile = await file.blob();
+
+    // return new Promise((resolve) => {
+    //   const reader = new FileReader();
+    //       reader.onloadend = () => {
+    //         console.log("web: " + reader.result.slice(0, 50));
+    //         resolve(reader.result as string);
+    //       };
+    //   reader.readAsDataURL(readFile);
+    // });
   }
 
   // Delete picture by removing it from reference data and the filesystem
