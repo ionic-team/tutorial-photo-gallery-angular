527fbb43095974e81734e7eb838cdd5ce3608ecf
Matt Netkow <matt.netkow@gmail.com>
2020-01-30 14:18:14 -0600

    remove ionic native plugins, add dark mode, add new explore component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { Plugins } from '@capacitor/core';
 const { SplashScreen } = Plugins;
 
